2e0e52a0588e54f76d8d4ff96c7d031ce098be32
edarnige <eden@enki>
2020-03-13 10:34:28 +0100

    Initialize Django and venv

---
+++ venv_viti/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/eden/Project_VitiVir/vitiVir/venv_viti/bin/python3.6
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ vitiVir_project/vitiVir_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class VitivirAppConfig(AppConfig):
+    name = 'vitiVir_app'
