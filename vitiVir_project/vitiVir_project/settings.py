36144cbaed6c8cd8ddb79346db3cf51309a08d4c
edarnige <eden@enki>
2020-03-13 14:16:29 +0100

    Connect MongoDB using djongo

--- vitiVir_project/vitiVir_project/settings.py
+++ vitiVir_project/vitiVir_project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'vitiVir_app'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,8 @@ WSGI_APPLICATION = 'vitiVir_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'djongo',
+        'NAME': 'test',
     }
 }
 
